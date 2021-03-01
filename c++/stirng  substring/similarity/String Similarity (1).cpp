b063b8e3d0c62ad9066742921c5540434cc9b658
abhinav <gaurabhinav54@gmail.com>
2021-03-01 19:22:37 +0530

    add code

---
+++ c++/stirng  substring/similarity/String Similarity (1).cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int x; cin>>x;
+  while(x--){
+    int n,a; 
+    cin>>n;
+    string v;
+    cin>>v;
+      for(int i=0;i<v.length();i=i+2){
+        cout<<v[i];
+      }
+      cout<<endl;
+      }
+      
+      }
+      
